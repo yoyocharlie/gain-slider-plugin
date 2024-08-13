3e961272b93b8ace5e68e5ea7ecdb711cf01097e
Jacob Pixler <jacobpixlerdev@gmail.com>
2024-08-12 19:46:04 -0600

    Categorized AAX as a dynamic plugin

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -122,7 +122,7 @@
  #define JucePlugin_AAXProductId           JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_AAXCategory
- #define JucePlugin_AAXCategory            0
+ #define JucePlugin_AAXCategory            2
 #endif
 #ifndef  JucePlugin_AAXDisableBypass
  #define JucePlugin_AAXDisableBypass       0
